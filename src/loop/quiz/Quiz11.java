8f9241e667dd2d1982ea0219c1fb8ad59f6001ce
seongmin <sungmin990923@naver.com>
2024-07-02 16:56:19 +0900

    0702 별 다이아몬드 완성ㅋ

--- src/loop/quiz/Quiz11.java
+++ src/loop/quiz/Quiz11.java
@@ -15,12 +15,12 @@ public class Quiz11 {
 			System.out.println("");
 		}
 	
-	for(int a = 4; a >= 1; a--) {
+	for(int a = 5; a >= 1; a--) {
+		for(int c =5 ; c >=a; c--) {
+			System.out.print(" ");
+		}
 		for (int b = 1; b<=a; b++) {
 			System.out.print("*");
-		}
-		for(int c = 4; c >= a; c--) {
-			System.out.print(" ");
 		}
 		System.out.println();
 	}
